41bcb32be2f57855f8d078f63082a472eed837bf
David Chow <Davidchou521@qq.com>
2017-09-05 14:33:53 +0800

    tools.js 常用工具方法

--- tools.js
+++ tools.js
@@ -17,13 +17,10 @@
 
 (function (global, $, tools) {
     ////1.C#自带GCC压缩会改变tool变量名
-    //var tool = new tools();
+    var tool = new tools();
 
     ////2.将tool显式挂载到window下(不建议挂在window下tool被公开)
     //global.tool = new tools();
-
-    ////3.此处理能解决压缩不公开
-    eval("var tool = new tools()");
 
     //tools.js
     var _tools = {
@@ -234,25 +231,25 @@
 
     //判断当前value是否是正确的 电子邮箱地址(Email) 格式。
     this.IsEmail = function (value) {
-        str = that.IsNullOrEmpty(value) ? "" : String(value);
+        value = that.IsNullOrEmpty(value) ? "" : String(value);
         return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(value);
     };
 
     //判断当前value是否是正确的 邮政编码(中国) 格式。
     this.IsZipCode = function (value) {
-        str = that.IsNullOrEmpty(value) ? "" : String(value);
+        value = that.IsNullOrEmpty(value) ? "" : String(value);
         return /^[\d]{6}$/.test(value);
     };
 
     //验证中文
     this.IsChinese = function (value) {
-        str = that.IsNullOrEmpty(value) ? "" : String(value);
+        value = that.IsNullOrEmpty(value) ? "" : String(value);
         return /^[\u0391-\uFFE5]+$/i.test(value);
     };
 
     //验证英文
     this.IsEnglish = function (value) {
-        str = that.IsNullOrEmpty(value) ? "" : String(value);
+        value = that.IsNullOrEmpty(value) ? "" : String(value);
         return /^[A-Za-z]+$/i.test(value);
     };
 
@@ -293,7 +290,7 @@
         var str = that.IsNullOrEmpty(arguments[0]) ? "" : String(arguments[0]);
         if ($.isArray(arguments[1])) {
             for (var i = 0; i < arguments[1].length; i++) {
-                value = arguments[1][i] ? arguments[1][i] : "";
+                var value = arguments[1][i] ? arguments[1][i] : "";
                 str = str.replace(new RegExp("\\{" + i + "}", "gm"), value);
             }
         } else {
@@ -379,7 +376,7 @@
 
         //创建参数
         $.each(object, function (k, v) {
-            input = document.createElement("input");
+            var input = document.createElement("input");
             input.name = k;
             input.value = v;
             input.type = "hidden";
