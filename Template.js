c0a0b23b3d05c589387d4daf9cc618723a72f90a
David Chow <Davidchou521@qq.com>
2018-01-03 23:14:27 +0800

    js模板

--- Template.js
+++ Template.js
@@ -2,7 +2,7 @@
  * Created by user on 2018-01-03.
  * The naming specification follows the small hump nomenclature:the first single word begins with a lowercase letter.
  * [Modified Log]
- * 2018-01-03 20:35      user            创建模板
+ * 2018-01-03 20:35         user            line[>9<50]             创建模板
  *
  */
 
