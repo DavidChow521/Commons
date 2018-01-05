c4bc3a19fef9af0a4a03c4971f8ac7e61e49200e
David Chow <Davidchou521@qq.com>
2018-01-05 16:09:21 +0800

    js模板

--- Template/jQuery.js
+++ Template/jQuery.js
@@ -9,21 +9,23 @@
  *  例2：$(".class").helloWorld();
  */
 
+!(function (global, $, undefined) {
 
+    //例1：jQuery本身的扩展方法（全局扩展）
+    $.extend({
+        helloWorld: function () {
+            alert("世界你好！");
+        }
+    })
 
-//例1：jQuery本身的扩展方法（全局扩展）
-$.extend({
-    helloWorld: function () {
-        alert("世界你好！");
-    }
-})
-
-//例2： jQuery 所选对象扩展方法（当前扩展）jQuery.fn==jQuery.prototype
-$.fn.extend({
-    helloWorld: function (parameters) {
+    //例2： jQuery 所选对象扩展方法（当前扩展）jQuery.fn==jQuery.prototype
+    $.fn.extend({
+        helloWorld: function (parameters) {
+            $(this).text("世界你好！");
+        }
+    });
+    $.fn.helloWorld = function (parameters) {
         $(this).text("世界你好！");
     }
-});
-$.fn.helloWorld = function (parameters) {
-    $(this).text("世界你好！");
-}
+
+})(this, jQuery)
