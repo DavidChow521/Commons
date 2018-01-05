bcb9cfcfb4e4e73fc0b9999a5836e8a2a74401f2
David Chow <Davidchou521@qq.com>
2018-01-05 10:10:48 +0800

    模板

---
+++ Template/jQuery.js
@@ -0,0 +1,7 @@
+/**
+ * Created by user on 2018-01-04.
+ * The naming specification follows the small hump nomenclature:the first single word begins with a lowercase letter.
+ * [Modified Log]
+ * yyyy-MM-dd HH:mm         user            line[number]             description
+ *
+ */
